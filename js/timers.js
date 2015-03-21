ec2bf7ab69146045dd401cdf7be7bed9734729ff
David Taylor <dtaylor@wavetronix.com>
2015-03-21 17:44:07 -0600

    made dynamic tabs for timers

--- js/timers.js
+++ js/timers.js
@@ -1,8 +1,8 @@
 function open_panel()
 {
 slideIt();
-var a=document.getElementById("sidebar");
-a.setAttribute("id","sidebar1");
+var a=document.getElementById("timerconfig");
+a.setAttribute("id","timerconfig1");
 a.setAttribute("onclick","close_panel()");
 }
 
@@ -20,8 +20,8 @@ function slideIt()
 	
 function close_panel(){
 slideIn();
-a=document.getElementById("sidebar1");
-a.setAttribute("id","sidebar");
+a=document.getElementById("timerconfig1");
+a.setAttribute("id","timerconfig");
 a.setAttribute("onclick","open_panel()");
 }
 
@@ -35,4 +35,27 @@ function slideIn()
 		slidingDiv.style.right = parseInt(slidingDiv.style.right) - 2 + "px";
 		setTimeout(slideIn, 1);	
 	}
+}
+var timercount = 0;
+function addTimer(){
+    if(timercount < 5 )
+    {
+        timercount += 1;
+        var timers = document.getElementById("timers");
+
+        var newtimer = document.createElement("div");
+        newtimer.className = "timer";
+
+        /*var image = document.createElement("img");
+        image.src = "img/timer-settings.png";
+        image.height = 50;
+        image.width = 50;
+
+        newtimer.appendChild(image);*/
+
+        timers.appendChild(newtimer);
+    }
+}
+function removeAllTimers(){
+
 }

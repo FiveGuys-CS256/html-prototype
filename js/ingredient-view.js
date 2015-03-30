f27864335cedba651974fca4890efac6b493ddbe
Kyle Pontius <kbpontius@gmail.com>
2015-03-30 00:18:25 -0600

    Added functionality to the Ingredient View.

---
+++ js/ingredient-view.js
@@ -0,0 +1,19 @@
+function populateIngredientList(name) {
+	switch (name) {
+	case "Lobster Mac":
+	  alert("lobster");
+	 	break;
+	case "New York Cheese Cake":
+	  alert("cheesecake");
+	  break;
+	case "Artisan Bread":
+	  alert("bread");
+	  break;
+	case "Family Quiche":
+	  alert("quiche");
+	  break;
+	default:
+	  alert("None found");
+	  break;
+	}
+}

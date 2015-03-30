9d4e83c65484c94bd032d575bab07d67ef5ca5f3
James Rowe <jroweboy@gmail.com>
2015-03-30 12:31:54 -0600

    Greatly reduced javascript code for select a meal and fixed a bug that kept the single recipe image from loading

--- js/select-meal.js
+++ js/select-meal.js
@@ -2,26 +2,7 @@ var pictures = {};
 
 window.addEventListener("DOMContentLoaded", function() {
     var modal = document.getElementById("meal-modal");
-    var container = document.createElement("div");
-    container.classList.add("modal-dialog");
-    container.style = 'width:1100px; height:600px;';
-    var header = document.createElement("div");
-    header.classList.add("modal-header");
-    var title = document.createElement("div");
-    title.classList.add("title");
-    title.classList.add("modal-title");
-    title.appendChild(document.createTextNode("Select A Meal"));
-    header.appendChild(title);
-    container.appendChild(header);
-
-    var body = document.createElement("div");
-    body.classList.add("modal-body");
-    body.id = "select-recipe-body";
-    var labs = document.createElement("div");
-    labs.classList.add("side-bar");
-    var labs_list = document.createElement("ul");
-    labs_list.classList.add("side-bar-list");
-    labs_list.id = "meal-modal-labs";
+    var labs_list = document.getElementById("meal-modal-labs");
     Object.keys(allRecipes).forEach(function (key) {
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(key));
@@ -39,18 +20,6 @@ window.addEventListener("DOMContentLoaded", function() {
         });
         labs_list.appendChild(li);
     });
-    labs.appendChild(labs_list);
-    body.appendChild(labs);
-//               <div class="picture-layout">
-//                   <div>
-//                   <div class="picture"><img src="http://www.dzai.com.br/static/conteudos/2013/05/14/99/99872/posts/61826a4f3540c85560e7cf5616574e75.jpg">
-//                       <h2>
-//                           New York Cheesecake
-//                       </h2>
-//                   </div>...
-    var pics = document.createElement("div");
-    pics.id = "meal-modal-recipes";
-    pics.classList.add("picture-layout");
 
     Object.keys(allRecipes).forEach(function (key) {
         var elm = document.createElement("div");
@@ -67,20 +36,4 @@ window.addEventListener("DOMContentLoaded", function() {
         }
         pictures[key] = elm;
     });
-
-    body.appendChild(pics);
-    container.appendChild(body);
-
-        // <div class="modal-footer">
-        //     <a href="#close" class="btn">Start</a> 
-        // </div>
-    var footer = document.createElement("div");
-    footer.classList.add("modal-footer");
-    var close_button = document.createElement("a");
-    close_button.href = "#close";
-    close_button.classList.add("btn");
-    close_button.appendChild(document.createTextNode("Start"));
-    footer.appendChild(close_button);
-    container.appendChild(footer);
-    modal.appendChild(container);
 });

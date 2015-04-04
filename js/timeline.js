680b936922e3ce7ad9ea7481279faa94d2ed9158
liukaichi <liukaichi@Gmail.com>
2015-04-04 16:37:06 -0600

    A somewhat working timeline populator. Timer images still arent' working.

---
+++ js/timeline.js
@@ -0,0 +1,22 @@
+/*example to use
+ alert("total time: " + allRecipes["Lab 1"][0]['timer']['5:00'] + "   " + allRecipes["Lab 1"][0]['timedInstructions']['5:00']);*/
+
+
+/*
+ var timeline = document.getElementById("timeline");
+ for (var recipeNo = 0; recipeNo < 4; recipeNo++) {                          *//*per recipe loop*//*
+
+ var instruction = allRecipes[labNo][recipeNo]["timedInstructions"];
+
+ for (var i = 0; i < instruction.length; i++) {                           *//*per time row loop*//*
+ var output = "<tr><td>" + allRecipes[labNo][recipeNo]["timedInstructions"][i] + "</td></tr>";
+ var step = document.getElementbyId("timeline");
+ timeline.innerHTML = output;
+ *//*timeline.appendChild(step);
+ *//* }*/
+
+/*
+
+function populateTimeline(labNo) {
+    alert(labNo);
+}*/

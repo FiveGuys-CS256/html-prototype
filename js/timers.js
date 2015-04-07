f8736b51f89b92f9cad050027b1f9d7407dc9318
longlostbro <longlostbro@gmail.com>
2015-04-06 21:05:10 -0600

    added dialog when trying to cancel a timer

--- js/timers.js
+++ js/timers.js
@@ -136,8 +136,7 @@ function addTimer(title, minute, second) {
         cancelTimerButton.innerHTML="Cancel";
         cancelTimerButton.addEventListener("click", function(){
             this.parentNode.dataset.state = "un-selected";
-            clearInterval(timerMap[this.id]);
-            document.getElementById("timers").removeChild(this.parentNode);
+            Confirm.render('Cancel Timer?','cancelTimer',this.parentNode);
         }, false);
 
         timerCountdown.id="timer"+currentTimerCount;
@@ -155,6 +154,10 @@ function addTimer(title, minute, second) {
             fireEvent(timerTime, "change");
         }
     }
+}
+function cancelTimer(timer){
+    clearInterval(timerMap[timer.id]);
+    document.getElementById("timers").removeChild(timer);
 }
 function fireEvent(element,event){
     if (document.createEventObject){
@@ -263,3 +266,59 @@ function startTimer(duration, display) {
     timerMap[display.id]=setInterval(timer, 1000);
 }
 var timerMap ={};
+
+function CustomAlert(){
+    this.render = function(dialog){
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH+"px";
+        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+        document.getElementById('dialogboxhead').innerHTML = "Acknowledge This Message";
+        document.getElementById('dialogboxbody').innerHTML = dialog;
+        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Alert.ok()">OK</button>';
+    }
+    this.ok = function(){
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    }
+}
+var Alert = new CustomAlert();
+function deletePost(id){
+    var db_id = id.replace("post_", "");
+    // Run Ajax request here to delete post from database
+    document.body.removeChild(document.getElementById(id));
+}
+function CustomConfirm(){
+    this.render = function(dialog,op,id){
+        var winW = window.innerWidth;
+        var winH = window.innerHeight;
+        var dialogoverlay = document.getElementById('dialogoverlay');
+        var dialogbox = document.getElementById('dialogbox');
+        dialogoverlay.style.display = "block";
+        dialogoverlay.style.height = winH+"px";
+        dialogbox.style.left = (winW/2) - (550 * .5)+"px";
+        dialogbox.style.top = "100px";
+        dialogbox.style.display = "block";
+
+        document.getElementById('dialogboxhead').innerHTML =dialog;
+        document.getElementById('dialogboxfoot').innerHTML = '<button onclick="Confirm.yes(\''+op+'\',\''+id+'\')">Yes</button> <button onclick="Confirm.no()">No</button>';
+    }
+    this.no = function(){
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    }
+    this.yes = function(op,id){
+        if(op == "cancelTimer"){
+            cancelTimer(document.querySelector(".timer[data-state='selected']"));
+        }
+        document.getElementById('dialogbox').style.display = "none";
+        document.getElementById('dialogoverlay').style.display = "none";
+    }
+}
+var Confirm = new CustomConfirm();
+
